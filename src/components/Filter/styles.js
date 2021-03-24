28c8a395cbfc5178d32c40b9b4f0e69ead2abc4a
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-03-24 17:33:50 -0300

    Adicionando o slider de valores

--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -1,3 +1,4 @@
+import ReactSlider from 'react-slider';
 import styled from 'styled-components';
 
 export const ContainerFilter = styled.div`
@@ -11,17 +12,19 @@ export const ContainerFilter = styled.div`
 
 export const ContainerPrice = styled.div`
   width: 119px;
-  margin-bottom: 23px;
+  margin-top: 18px;
+  margin-bottom: 38px;
 `;
 
 export const TitlePrice = styled.p`
   font-family: Montserrat;
   font-weight: 600;
   font-size: 18px;
+  margin-bottom: 18px;
 `;
 
 export const SubtitlePrice = styled.p`
-display: inline-block;
+display: block;
 font-family: Montserrat;
 font-weight: 300;
 font-size: 16px;
@@ -46,9 +49,10 @@ export const CheckMarkRadio = styled.span`
 export const Input = styled.input``;
 
 export const ContainerRadio = styled.label`
-  display: inline;
+  display: block;
   position: relative;
   padding-left: 28px;
+  margin-bottom: 18px;
   cursor: pointer;
   -webkit-user-select: none;
   -moz-user-select: none;
@@ -86,12 +90,12 @@ export const Line = styled.hr`
 export const ContainerValue = styled.div`
   width: 100%;
   margin-top: 33px;
-  display: flex;
-  justify-content: space-between;
   box-sizing: border-box;
 `;
 
 export const ContainerTextValue = styled.div`
+    display: flex;
+    justify-content: space-between;
 `;
 
 export const NumberValue = styled.h1`
@@ -103,6 +107,8 @@ export const NumberValue = styled.h1`
 export const ContainerCategory = styled.div`
 display: flex;
 flex-direction: column;
+margin-top: 33px;
+margin-bottom: 24px;
 
 `;
 
@@ -112,18 +118,19 @@ export const Checkmark = styled.span`
   position: absolute;
   top: 0;
   left: 0;
-  height: 25px;
-  width: 25px;
-  background-color: #eee;
+  height: 17px;
+  width: 17px;
+  border: 1px solid #999999;
+  background-color: white;
   border-radius: 20%;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const ContainerCheckbox = styled.div`
-  display: inline-flex;
-  align-items: flex-start;
+export const ContainerCheckbox = styled.label`
+  display: block;
+  margin-bottom: 16px;
   position: relative;
   cursor: pointer;
   padding-left: 26px;
@@ -161,11 +168,13 @@ export const ContainerCheckbox = styled.div`
 `;
 
 export const ContainerRating = styled.div`
+margin-top: 33px;
 `;
 
 export const ContainerStars = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 17px;
 `;
 
 export const Rating = styled.h1`
@@ -189,4 +198,29 @@ export const Button = styled.button`
   padding: 17px 74px 18px 53px;
   border-radius: 8px; 
   box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
+`;
+
+export const StyledSlider = styled(ReactSlider)`
+    width: 100%;
+    height: 25px;
+`;
+
+export const FilterThumb = styled.div`
+  height: 20px;
+  width: 20px;
+  text-align: center;
+  position: relative;
+  top: -35%;
+  border-radius: 50%;
+  cursor: grab;
+  background-color: #606060;
+  outline: none;
+`;
+
+export const FilterTrack = styled.div`
+  top: 0;
+  bottom: 0;
+  background-color: #606060;
+  border-radius: 999px;
+  height: 6px;
 `;
