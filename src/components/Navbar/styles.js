82ed96c0fa2a3a66c033c337a2fcccba1e388ecb
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2021-03-30 21:07:24 -0300

    Navbar finalizada

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,65 +1,51 @@
 import styled from "styled-components";
 
 export const ContainerNavbar = styled.div`
-  width: 280px;
-  height: 1960px;
-  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.170509);
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 62px;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.135216);
+    border-radius: 8px;
+    
 `;
 
-export const ContainerLogo = styled.div`
-  display: flex;
-  justify-content: center;
+
+export const IconsNavbar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
 `;
 
-export const Logo = styled.div`
-  margin-top: 29px;
+export const IconsLeft = styled.div`
+    display: flex;
+    gap: 22px;
+    margin-left: 22px;
 `;
 
-export const ContainerDropdown = styled.div`
-  display: flex;
-  margin-left: 24px;
-  margin-top: 34px;
+export const IconsRight = styled.div`
+    display: flex;
+    gap: 22px;
+    margin-right: 19px;
+`;
 
-  select{
+export const Client = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ClientName = styled.p`
     font-family: Montserrat;
-    font-size: 15px;
-    border: none;
-    background: #ECECEC;
-    border-radius: 4px;
-    height: 36px;
-    width: 236px;
-  }
+    font-size: 14px;
+    font-weight: 400;
+    color: #2C2C2C;
 `;
 
-export const ContainerApps = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 38px;
-`;
-
-export const TitleApps = styled.p`
-  font-size: 14px;
-  color: #999999;
-  font-weight: 400;
-  font-family: Montserrat;
-  margin-top: 20px;
-`;
-
-export const ContainerAnchors = styled.div`
-  margin-top: 20px;
-  font-family: Montserrat;
-  display: flex;
-  flex-direction: column;
-  color: #2c2c2c;
-  a {
-    text-decoration: none;
-    margin-bottom: 8px;
-  }
-  img {
-    margin-right: 10px;
-  }
-
-  a:visited {
-    color: #2c2c2c;
-  }
+export const ClientSubtitle = styled.p`
+    font-family: Montserrat;
+    font-size: 14px;
+    font-weight: 300;
+    color: #2C2C2C;
 `;
